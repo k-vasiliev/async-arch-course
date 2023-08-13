af9dc33cf3c2dbc29d7826a39904967c8ee3d7e1
Kirill Vasilev <ki.vasilyev@hh.ru>
2023-08-13 20:47:33 +0400

    add services stubs

---
+++ task-tracker/src/main/java/ru/vasilev/asyncarccourse/AsyncArcCourseApplication.java
@@ -0,0 +1,13 @@
+package ru.vasilev.asyncarccourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncArcCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncArcCourseApplication.class, args);
+	}
+
+}
